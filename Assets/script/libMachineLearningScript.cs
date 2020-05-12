db617886ee2a6379afc5d0512660c4b1b875b690
SebastiDELB <sebas9241@hotmail.fr>
2020-05-12 16:09:40 +0200

    scène 1 et les premeirs pas

---
+++ Assets/script/libMachineLearningScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class libMachineLearningScript: MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("With VS DLL : ");
+        Debug.Log(libMachineLearningWrapper.my_add(42, 51));
+        Debug.Log(libMachineLearningWrapper.my_mul(2, 3));
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
---
+++ Assets/script/libMachineLearningWrapper.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.InteropServices;
+
+public static class libMachineLearningWrapper
+{
+    [DllImport("libMachineLearning")]
+    public static extern int my_add(int x, int y);
+
+    [DllImport("libMachineLearning")]
+    public static extern int my_mul(int x, int y);
+}
